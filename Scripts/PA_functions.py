cc8011dda9080bd4bbc9e0f95dde6872ba3fc01b
Leonardo <rlsandovalp@unal.edu.co>
2021-11-12 14:13:54 +0100

    Add first script

---
+++ Scripts/PA_functions.py
@@ -0,0 +1,279 @@
+import numpy as np
+from scipy.optimize import minimize, nnls, fmin_slsqp
+import matplotlib.pyplot as plt
+import math
+
+plt.style.use(['science','nature'])
+
+# FUNCTIONS OUR NOVEL METHOD
+
+def ouropt_1(unknowns, def_operations, em_peaks_mean, um_peaks_mean, tipo, cv, ignorar, nEM):
+    # Function to compute the loss function for a given value of cv. This function is used to obtain optimal values of x and MR.
+    mini = 0
+    for q, _ in enumerate(def_operations):
+        if len(set(def_operations[q][0]).intersection(ignorar))+len(set(def_operations[q][1]).intersection(ignorar))>0: 
+            continue
+        Rij, rij = compute_ratios_our(0, 0, unknowns, nEM, em_peaks_mean, um_peaks_mean, q, def_operations,tipo)
+        phi = (1/cv)*(1+rij/Rij)/(math.sqrt(2*(1+(rij/Rij)**2)))
+        gam = math.sqrt(math.pi)*phi*math.exp(phi**2)*math.erf(phi)
+        uno = math.log(1+(rij/Rij)**2)
+        dos = math.log(1+gam)
+        mini = mini + uno - dos
+    return mini
+
+def ouropt_2(cv, unknowns, def_operations, em_peaks_mean, um_peaks_mean, tipo, ignorar, nEM):
+    # Function to compute the loss function for a given value of x, and MR. This function is used to obtain optimal values of cv.
+    mini = 0
+    ignorados = 0
+    for q, _ in enumerate(def_operations):
+        if len(set(def_operations[q][0]).intersection(ignorar))+len(set(def_operations[q][1]).intersection(ignorar))>0: 
+            ignorados = ignorados + 1
+            continue
+        Rij, rij = compute_ratios_our(0, 0, unknowns, nEM, em_peaks_mean, um_peaks_mean, q, def_operations,tipo)
+        phi = (1/cv)*(1+rij/Rij)/(math.sqrt(2*(1+(rij/Rij)**2)))
+        gam = math.sqrt(math.pi)*phi*math.exp(phi**2)*math.erf(phi)
+        uno = math.log(1+gam)
+        mini = mini + uno
+    mini = -mini + (len(def_operations)-ignorados)/(cv**2)
+    return mini
+
+def compute_ratios_our(Num, Den, unknowns, nEM, em_peaks_mean, um_peaks_mean, q, def_operations, tipo):
+    # Function to read the experimental ratios and compute the analytical ratios for OUR objective function
+    um_peaks_mean = np.reshape(um_peaks_mean, -1)
+    if tipo[q] == '1/1':
+        for em in range(nEM-1):
+            Num = Num+unknowns[em]*unknowns[-nEM+1+em]*em_peaks_mean[em,def_operations[q][0][0]-1]
+            Den = Den+unknowns[em]*unknowns[-nEM+1+em]*em_peaks_mean[em,def_operations[q][1][0]-1]
+        Num = Num + unknowns[em+1]*em_peaks_mean[em+1,def_operations[q][0][0]-1]
+        Den = Den + unknowns[em+1]*em_peaks_mean[em+1,def_operations[q][1][0]-1]
+        Hi = um_peaks_mean[def_operations[q][0][0]-1]
+        Hj = um_peaks_mean[def_operations[q][1][0]-1]
+        Rij = Hi/Hj
+    if tipo[q] == '1/2':
+        for em in range(nEM-1):
+            Num = Num+unknowns[em]*unknowns[-nEM+1+em]*em_peaks_mean[em,def_operations[q][0][0]-1]
+            Den = Den+unknowns[em]*unknowns[-nEM+1+em]*(em_peaks_mean[em,def_operations[q][1][0]-1]+em_peaks_mean[em,def_operations[q][1][1]-1])
+        Num = Num + (100-np.sum(unknowns[0:nEM-1]))*em_peaks_mean[em+1,def_operations[q][0][0]-1]
+        Den = Den + (100-np.sum(unknowns[0:nEM-1]))*(em_peaks_mean[em+1,def_operations[q][1][0]-1]+em_peaks_mean[em+1,def_operations[q][1][1]-1])
+        Hi = um_peaks_mean[def_operations[q][0][0]-1]
+        Hj = um_peaks_mean[def_operations[q][1][0]-1]
+        Hk = um_peaks_mean[def_operations[q][1][1]-1]
+        Rij = Hi/(Hj+Hk)
+    if tipo[q] == '1/3':
+        for em in range(nEM-1):
+            Num = Num+unknowns[em]*unknowns[-nEM+1+em]*em_peaks_mean[em,def_operations[q][0][0]-1]
+            Den = Den+unknowns[em]*unknowns[-nEM+1+em]*(em_peaks_mean[em,def_operations[q][1][0]-1]+em_peaks_mean[em,def_operations[q][1][1]-1]+em_peaks_mean[em,def_operations[q][1][2]-1])
+        Num = Num + (100-np.sum(unknowns[0:nEM-1]))*em_peaks_mean[em+1,def_operations[q][0][0]-1]
+        Den = Den + (100-np.sum(unknowns[0:nEM-1]))*(em_peaks_mean[em+1,def_operations[q][1][0]-1]+em_peaks_mean[em+1,def_operations[q][1][1]-1]+em_peaks_mean[em,def_operations[q][1][2]-1])
+        Hi = um_peaks_mean[def_operations[q][0][0]-1]
+        Hj = um_peaks_mean[def_operations[q][1][0]-1]
+        Hk = um_peaks_mean[def_operations[q][1][1]-1]
+        Hl = um_peaks_mean[def_operations[q][1][2]-1]
+        Rij = Hi/(Hj+Hk+Hl)
+    if tipo[q] == '2/3':
+        for em in range(nEM-1):
+            Num = Num+unknowns[em]*unknowns[-nEM+1+em]*(em_peaks_mean[em,def_operations[q][0][0]-1]+em_peaks_mean[em,def_operations[q][0][1]-1])
+            Den = Den+unknowns[em]*unknowns[-nEM+1+em]*(em_peaks_mean[em,def_operations[q][1][0]-1]+em_peaks_mean[em,def_operations[q][1][1]-1]+em_peaks_mean[em,def_operations[q][1][2]-1])
+        Num = Num + (100-np.sum(unknowns[0:nEM-1]))*(em_peaks_mean[em+1,def_operations[q][0][0]-1]+em_peaks_mean[em+1,def_operations[q][0][1]-1])
+        Den = Den + (100-np.sum(unknowns[0:nEM-1]))*(em_peaks_mean[em+1,def_operations[q][1][0]-1]+em_peaks_mean[em+1,def_operations[q][1][1]-1]+em_peaks_mean[em+1,def_operations[q][1][2]-1])
+        Hi = um_peaks_mean[def_operations[q][0][0]-1]
+        Hj = um_peaks_mean[def_operations[q][0][1]-1]
+        Hk = um_peaks_mean[def_operations[q][1][0]-1]
+        Hl = um_peaks_mean[def_operations[q][1][1]-1]
+        Hm = um_peaks_mean[def_operations[q][1][2]-1]
+        Rij = (Hi+Hj)/(Hk+Hl+Hm)
+    rij=Num/Den
+    return Rij, rij
+
+def convert_operations(operations_path):
+    # Function to convert the operations file to readable tables nratios, w_r
+    with open(operations_path, 'r') as f: operations = f.read().splitlines()
+    def_operations = []
+    new_list = []
+    n_p = []
+    for q, operation in enumerate(operations):
+        def_operations.append([])
+        d=0
+        def_operations[q].append([])
+        multidigit = ''
+        for character in operation:
+            if character == '(':
+                next
+            elif character.isnumeric():
+                multidigit += character
+            elif character == '+' or character == '-' or character == ')':
+                def_operations[q][d].append(int(multidigit))
+                multidigit = ''
+            elif character == '/':  
+                d=1
+                def_operations[q].append([])
+
+    for h in def_operations:
+        for i in h:
+            for j in i:
+                new_list.append(j)
+    a = len(new_list)
+    for i in range(max(new_list)):
+        n_p.append(new_list.count(i+1))
+    
+    #Weight and type of each ratio (uncertainty propagation is computed with different formulas depending on the type of ratio)
+    tipo = []
+    for operation in def_operations:
+        tipo.append(str(len(operation[0]))+'/'+str(len(operation[1])))
+    return def_operations, tipo
+
+
+
+# f_normalize normalize a given set of peaks based on the parameter normalize and separate the end-members from the mixture
+def f_normalize(normalize,peaks_averaged,end_members):
+    if normalize == 1:                                
+        peaks_normalized = peaks_averaged/np.mean(peaks_averaged, axis=0)
+    else:
+        peaks_normalized = peaks_averaged
+    return np.transpose(peaks_normalized[0:end_members,:]), np.transpose(peaks_normalized[end_members:,:])
+
+# f_normalize normalize for ALS
+def f_normalize_als(normalize,b):
+    if normalize == 1:
+        for i in range(b.shape[0]):                                
+            b[i,:] = b[i,:]/np.max(b[i,:])
+    else:
+        b = b
+    return b
+
+# f_calcula performs least squares and save the info in the selected lists
+def f_calcula(p_emr,p_mr,saving,end_members):
+    calcula = np.linalg.lstsq(p_emr, p_mr)[0]
+    # np.matmul(np.matmul(np.linalg.inv(np.matmul(np.transpose(p_emr),p_emr)),np.transpose(p_emr)),p_mr)
+    for l in range(end_members):
+        saving[l].append(calcula[l])
+    return(saving)
+
+# f_calcula constrained and bounded
+def f_calcula_opt(p_emr,p_mr,saving,end_members,x0,residuals,eq_cond,bounds):
+    A = p_emr
+    b = p_mr
+    calcula = fmin_slsqp(residuals, x0, bounds = bounds, eqcons = [eq_cond], args = (A, b), iprint = -1)
+    for l in range(end_members):
+        saving[l].append(calcula[l])
+    return(saving)
+
+# preprocess delete rows with high average cv and repetitions causing large cv
+def preprocess(table,pp,max_cv_peaks,max_cv_samples):
+    if pp == 1:
+        #################################     Delete the peaks whose CV is larger than threshold value ##############################
+        auxiliar_df = (table.std(level=0, ddof = 1)*100/table.mean(level=0)).max(axis = 0)
+        matar = auxiliar_df[auxiliar_df>max_cv_peaks].index
+        for i in matar: 
+            table.drop(i, axis = 1, inplace = True)
+
+        #################################     Identify repetitions causing large CV and delete them ##############################
+        auxiliar_df = (table.std(level=0)*100/table.mean(level=0)).mean(axis = 1)
+        revisar = auxiliar_df[auxiliar_df>max_cv_samples].index
+        table.reset_index(inplace = True)
+        for i in revisar:
+            counter = np.argmax(np.mean(abs(table[table['Mix']==i].values[:,1:]-np.mean(table[table['Mix']==i].values[:,1:],axis = 0)), axis = 1))
+            position_i = auxiliar_df.index.tolist().index(i)
+            rep = table[table['Mix']==i].values.shape[0]
+            table.drop(position_i*rep + counter, inplace = True)
+        table.set_index('Mix')
+    return(table)
+
+# preprocess nouvelle
+def preprocess_nouvelle(table,pp,max_cv_peaks,max_cv_samples):
+    # print('- '*50)
+    if pp == 1:
+        #################################     Delete the peaks whose CV is larger than threshold value ##############################
+        auxiliar_df = (table.std(level=0, ddof = 1)*100/table.mean(level=0)).max(axis = 0)
+        ignorar = auxiliar_df[auxiliar_df>max_cv_peaks].index
+
+        #################################     Identify repetitions causing large CV and delete them ##############################
+        auxiliar_df = (table.std(level=0)*100/table.mean(level=0)).mean(axis = 1)
+        revisar = auxiliar_df[auxiliar_df>max_cv_samples].index
+        table.reset_index(inplace = True)
+        for i in revisar:
+            counter = np.argmax(np.mean(abs(table[table['Mix']==i].values[:,1:]-np.mean(table[table['Mix']==i].values[:,1:],axis = 0)), axis = 1))
+            position_i = auxiliar_df.index.tolist().index(i)
+            rep = table[table['Mix']==i].values.shape[0]
+            table.drop(position_i*rep + counter, inplace = True)
+        table.set_index('Mix', inplace = True)
+    return(table,list(map(int, ignorar.tolist())))
+
+# preprocess nouvelle
+def preprocess_our(table,pp,max_cv_samples, max_cv_peaks):
+    # print('- '*50)
+    if pp == 1:
+        #################################     Delete the peaks whose CV is larger than threshold value ##############################
+        auxiliar_df = ((table.std(level=0, ddof = 1))*100/table.mean(level=0)).max(axis = 0)
+        ignorar = auxiliar_df[auxiliar_df>max_cv_peaks].index
+
+        #################################     Identify repetitions causing large CV and delete them ##############################
+        auxiliar_df = (table.std(level=0)*100/table.mean(level=0)).mean(axis = 1)
+        revisar = auxiliar_df[auxiliar_df>max_cv_samples].index
+        table.reset_index(inplace = True)
+        for i in revisar:
+            counter = np.argmax(np.mean(abs(table[table['Mix']==i].values[:,1:]-np.mean(table[table['Mix']==i].values[:,1:],axis = 0)), axis = 1))
+            position_i = auxiliar_df.index.tolist().index(i)
+            rep = table[table['Mix']==i].values.shape[0]
+            table.drop(position_i*rep + counter, inplace = True)
+        table.set_index('Mix', inplace = True)
+    return(table,list(map(int, ignorar)))
+
+# Computes the best approximation of X based on a L1 norm
+def L1_norm(beta,p_emr,p_mr):
+    return np.linalg.norm(np.matmul(p_emr,beta)-p_mr, ord=1)
+
+# Computes the best approximation of X based on a L1 norm
+# def L1_norm_1(beta,p_emr,p_mr):
+#     print(np.linalg.norm(np.matmul(p_emr,beta)-p_mr, ord=1))
+#     return np.linalg.norm(np.matmul(p_emr,beta)-p_mr, ord=1)
+
+# Minimizes Y-beta*X and save tha info in the selected lists (I must try to constrain)
+def f_calcula_l1(p_emr, p_mr, saving,end_members):
+    beta = np.ones(end_members)*(1/end_members)
+    res = minimize(L1_norm,beta, args=(p_emr,p_mr))
+    calcula = res.x
+    for l in range(end_members):
+        saving[l].append(calcula[l])
+    return(saving)
+
+# Minimizes Y-beta*X and save tha info in the selected lists (I must try to constrain)
+def f_calcula_l1_cons(p_emr, p_mr, saving,end_members):
+    beta = np.ones(end_members)*(1/end_members)
+    bnds = ((0,1),(0,1),(0,1))
+    cons = ({'type': 'ineq', 'fun': lambda beta: np.sum(beta)-0.95})
+    res = minimize(L1_norm,beta, args=(p_emr,p_mr), bounds = bnds, constraints = cons)
+    calcula = res.x
+    for l in range(end_members):
+        saving[l].append(calcula[l])
+    return(saving)
+
+# Sort a 1D array
+def bogosort(x):
+    while np.any(x[:-1] > x[1:]):
+        np.random.shuffle(x)
+    return x
+
+
+# Plot confidence intervals
+def plot_results(X_todos,conf_int_inf,conf_int_sup,real_results):
+    factors = X_todos.shape[0]
+    mixtures = X_todos.shape[1]
+    r = np.zeros((factors,mixtures))
+    plt.figure(figsize=(mixtures+4, 4), dpi = 250)
+    barWidth = 0.3
+    yer = (conf_int_sup-conf_int_inf)/2
+    for factor in range(factors):
+        if factor == 0: 
+            r[factor,:] = np.arange(mixtures)
+        else:
+            r[factor,:] = [x + barWidth for x in r[factor-1,:]]
+        plt.bar(r[factor,:], X_todos[factor,:]*100, width = barWidth, edgecolor = 'black', yerr=yer[factor,:], capsize=7, label='EM'+str(factor+1))
+        if factor == 0:
+            plt.scatter(r[factor,:], real_results[:,factor], color = 'black', zorder=2, label = 'True values')
+        else:
+            plt.scatter(r[factor,:], real_results[:,factor], color = 'black', zorder=2)
+    plt.xticks([r + barWidth for r in range(mixtures)], ['M'+str(mixture + 1) for mixture in range(mixtures)])
+    plt.ylabel('Mass Fractions [\%]')
+    plt.legend()
+    plt.show()
---
+++ Scripts/our_2.py
@@ -0,0 +1,105 @@
+import numpy as np
+from scipy.optimize import minimize
+from scipy.optimize import Bounds
+import pandas as pd
+from itertools import combinations
+
+from PA_functions import plot_results, preprocess_our, ouropt_1, ouropt_2, convert_operations
+from scipy.optimize import LinearConstraint
+from scipy.optimize import fsolve
+
+###############################     MODIFY!!!  ##################################
+folder = 'G:/My Drive/05 - PhD/2_Product_Allocation/Data_Base/Third_group/NGS_J19_DRK/'      # Directory
+file = 'NGS_J19_DRK'                        # Name of the file
+use_all_peaks = 1                           # Use all peaks? (1 Yes, 0 No)
+peaks_to_analyze = 11                       # How many peaks use?
+delta = 0.05
+pp = 0                                      # Preprocess? (1 Yes, 0 No)
+max_cv_samples = 10                         # Max intrasample CV for repetitions
+max_cv_peaks = 20                           # Max cv allowed for the repetitions of a peak
+cv = 0.5
+#################################################################################
+
+################    HOW MANY END-MEMBERS, HOW MANY MIXTURES?    ########################
+dataset = pd.read_csv(folder+"/"+file+".csv").set_index('Mix')
+nEM = len([i for i in dataset.index.unique().values.tolist() if i.startswith('EM')])
+nMix = len([i for i in dataset.index.unique().values.tolist() if i.startswith('M')])
+
+################    PREPROCESSING    ########################
+if use_all_peaks == 0:
+    peaks = dataset.iloc[:,0:peaks_to_analyze+1]
+else:
+    peaks = dataset.iloc[:,:]
+if pp == 1: 
+    peaks, ignorar = preprocess_nameless(peaks,pp,max_cv_samples, max_cv_peaks)
+else:
+    ignorar = []
+
+################    WHERE ARE THE OPERATIONS?    ########################
+operations = 'operations_all.txt'
+if use_all_peaks == 0: 
+    operations = 'operations'+str(peaks_to_analyze)+'.txt'
+operations_path=folder+operations
+
+# Convert the operations from the text file to an array easier to manipulate
+def_operations, tipo = convert_operations(operations_path)
+
+# Define end members and mixtures list, then create combinations
+end_members = ['EM'+str(x+1) for x in range(nEM)]
+mixtures = ['M' + str(x+1) for x in range(nMix)]
+comb = list(combinations(mixtures, 1))
+
+# Create an array to storage the results
+X_todos = np.zeros((nEM, nMix))
+
+# For each combinations of mixtures
+for i in range(nMix):
+    # Define the unknown mixtures, create end members array, create unknown mixtures array
+    unknown_mixtures = mixtures[i]
+
+    # Average chromatograms
+    em_peaks_mean = dataset.loc[end_members].mean(level = 0)
+    um_peaks_mean = dataset.loc[unknown_mixtures].mean(level = 0)
+    if dataset.loc[unknown_mixtures].shape[0] == dataset.loc[unknown_mixtures].size:
+        um_peaks_mean = dataset.loc[unknown_mixtures]
+
+    # Define vector of unknowns and initialize its values, give an initial value to CV, and define bounds for the optimization
+    unknowns = np.ones(2*nEM-1)  # [X1, X2, X3, ..., MR1, MR2] 
+    lb = np.ones((nEM)+nEM-1)*(1-delta)
+    ub = np.ones((nEM)+nEM-1)*(1+delta)
+    lc = np.zeros((nEM)+nEM-1)
+    unknowns[:-(nEM-1)] = 100/nEM
+    lb[:-(nEM-1)] = 0
+    ub[:-(nEM-1)] = 100
+    lc[:-(nEM-1)] = 1
+    
+    bounds_x = Bounds(lb, ub)
+    bounds_cv = [(0.038,1)]
+    linear_constraint = LinearConstraint(lc.tolist(), [100], [100])
+
+    C = 10
+    while C > 0.01:
+    # Minimize objective function to obtain values of unknowns
+        res = minimize(ouropt_1, unknowns, method = 'SLSQP', constraints=linear_constraint, bounds = bounds_x, args = (def_operations, em_peaks_mean.values, um_peaks_mean.values, tipo, cv, ignorar, nEM))
+        unknowns = res.x
+        C1 = cv
+
+        # Update the value of the cv considering the values of the unknowns obtained in the previous step
+        res = minimize(ouropt_2, cv, method = 'SLSQP', bounds = bounds_cv, args = (unknowns, def_operations, em_peaks_mean.values, um_peaks_mean.values, tipo, ignorar, nEM))
+        cv = res.x 
+        C = abs(C1-cv)
+    # Print and save the results for each unknown mixture
+    if nEM == 2:
+        print(unknown_mixtures, unknowns[0], unknowns[1], cv)
+    elif nEM ==3:
+        print(unknown_mixtures, unknowns[0], unknowns[1], unknowns[2], cv)
+
+    for j in range(nEM):
+        X_todos[j,i] = unknowns[j]
+
+########### Load the real results, compute estimates+-5% and plot  ################
+real_results = pd.read_csv(folder+"/p_"+file+".csv", delimiter=',').values[nEM:,1:]
+conf_int_sup = X_todos/100 + 5
+conf_int_inf = X_todos/100 - 5
+# plot_results(X_todos/100,conf_int_inf,conf_int_sup,real_results)
+print(np.mean(abs(X_todos-np.transpose(real_results))))
